611258d29ca8d13236fc19f93f69bd23b79f924b
root <root@DESKTOP-0N365CC>
2023-02-25 00:01:37 -0300

    rota get  urls

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -4,13 +4,23 @@ import { db } from "../configs/database.js";
 export async function url(req, res) {
     const { url } = res.locals.url;
     const shortUrl = nanoid(8);
+    const text = '';
+    // generateCode()
+    // function generateCode() {
+    //    console.log("a")
+    //     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    //     for (let i = 0; i < 5; i++)
+    //         text += possible.charAt(Math.floor(Math.random() * possible.length));
+    //     return text;
+    // }
+
     try {
         await db.query(
             `
-          INSERT INTO urls ("shortUrl", url) 
-          VALUES ($1, $2);
+          INSERT INTO urls ("shortUrl", url, "shortenedUrls") 
+          VALUES ($1, $2, $3);
           `,
-            [shortUrl, url]
+            [shortUrl, url, text ]
         );
         const IdUrl = await db.query('SELECT * FROM urls WHERE url=$1', [url]);
 
@@ -25,10 +35,25 @@ export async function url(req, res) {
 
 export async function listUrl(req, res) {
     const id = Number(req.params.id);
-    
+
     try {
-        const gameExist = await db.query('SELECT * FROM urls WHERE id=$1', [id]);
-        res.status(200).send(gameExist.rows[0]);
+        const urlExist = await db.query('SELECT * FROM urls WHERE id=$1', [id]);
+        if (!urlExist.rowCount > 0) return res.sendStatus(404);
+        res.status(200).send(urlExist.rows[0]);
+
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+}
+
+export async function redirectUrl(req, res) {
+    const id = Number(req.params.id);
+
+    try {
+        const urlExist = await db.query('SELECT * FROM urls WHERE id=$1', [id]);
+        if (!urlExist.rowCount > 0) return res.sendStatus(404);
+        res.status(200).send(urlExist.rows[0]);
+        res.redirect(url)
     } catch (error) {
         res.status(500).send(error.message);
     }
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { url , listUrl} from "../controllers/url.controller.js";
+import { url , listUrl, redirectUrl} from "../controllers/url.controller.js";
 import { validSchemaUrl } from "../middlewares/url.middleware.js";
 
 const urlRouter = Router();
 
 urlRouter.post("/urls/shorten", validSchemaUrl, url);
 urlRouter.get("/urls/:id",  listUrl);
+urlRouter.get("/urls/open/:shortUrl",  redirectUrl);
+
 
 
 export default urlRouter;
